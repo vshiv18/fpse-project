f6d45c5a16786f4f058d56dd18f958014f5a2dbf
drnatebrown <drnatebrown@gmail.com>
2023-11-17 10:37:28 -0500

    Add prev_hash function

--- src/hash.mli
+++ src/hash.mli
@@ -1,6 +1,10 @@
+(** Rolling hash returns the hash over a window size, implicitly assumed to be the length of the
+    string given in hash; next and prev hash values are found by rolling the hash left/right **)
 module type S = sig
   val hash : string -> int
   val next_hash : string -> int -> int
+  val prev_hash : string -> int -> int
+  (** Returns true if the [hash str] is 0 **)
   val is_trigger_string : string -> bool
 end
 
