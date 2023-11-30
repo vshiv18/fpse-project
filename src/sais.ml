2282b27fd8ddddd7469bd8ec1b69c066f9a19a50
drnatebrown <drnatebrown@gmail.com>
2023-11-30 17:53:01 -0500

    Add methods for filling buckets with LMS and induced sorting L/S

--- src/sais.ml
+++ src/sais.ml
@@ -1,20 +1,40 @@
 open Core
 
-module SAIS (Key_type : Base.Hashtbl.Key.S)= struct
+module SAIS (Key_type : Base.Hashtbl.Key.S) = struct
   type t = int Array.t
 
-  let make_buckets (text : 'a Array.t) :
-      t * ('a, int) Hashtbl.t * ('a, int) Hashtbl.t * ('a, int * int) Hashtbl.t
-      =
+  let make_buckets (text : 'a Array.t) : t * ('a, int * int) Hashtbl.t =
     let array = Array.create ~len:(Array.length text + 1) (-1) in
     let tbl =
       text |> Array.to_list
-      |> Hashtbl.group (module Key_type)
+      |> Hashtbl.group
+           (module Key_type)
            ~get_key:(fun x -> x)
            ~get_data:(fun _ -> 1)
            ~combine:(fun x y -> x + y)
     in
     (array, tbl)
+
+  let set_bucket (buckets : t) (idx : int) (v : 'a)
+      (empty_slots : ('a, int * int) Hashtbl.t) (typ : char) =
+    let l, r = Hashtbl.find_exn empty_slots v in
+    let arr =
+      match typ with
+      | 'L' ->
+          let () =
+            Hashtbl.update empty_slots v ~f:(fun x ->
+                match x with Some (l, r) -> (l + 1, r) | None -> (0, 0))
+          in
+          l
+      | 'S' ->
+          let () =
+            Hashtbl.update empty_slots v ~f:(fun x ->
+                match x with Some (l, r) -> (l, r + 1) | None -> (0, 0))
+          in
+          r
+      | _ -> raise @@ Invalid_argument "Incorrect type: L/S expected"
+    in
+    Array.set buckets arr idx
 
   let getSL (text : 'a Array.t) : string =
     let rec helper (sl : char list) (pos : int) : char list =
@@ -31,11 +51,23 @@ module SAIS (Key_type : Base.Hashtbl.Key.S)= struct
   let add_LMS (text : 'a Array.t) (sl : string) (buckets : t)
       (empty_slots : ('a, int * int) Hashtbl.t) =
     Array.iteri text ~f:(fun idx v ->
-        if isLMS sl idx then
-          let l, _ = Hashtbl.find_exn empty_slots v in
-          let () = Array.set buckets l idx in
-          Hashtbl.update empty_slots v ~f:(fun x ->
-              match x with Some (l, r) -> (l + 1, r) | None -> (0, 0)))
+        if isLMS sl idx then set_bucket buckets idx v empty_slots 'S')
+
+  let induce_L (text : 'a Array.t) (sl : string) (buckets : t)
+      (empty_slots : ('a, int * int) Hashtbl.t) =
+    Array.iter buckets ~f:(fun ele ->
+        match ele with
+        | i when i - 1 > 0 && Char.equal (String.get sl (i - 1)) 'L' ->
+            set_bucket buckets (i - 1) (Array.get text (i - 1)) empty_slots 'L'
+        | _ -> ())
+
+  let induce_S (text : 'a Array.t) (sl : string) (buckets : t)
+  (empty_slots : ('a, int * int) Hashtbl.t) =
+    Array.fold_right text ~init:() ~f:(fun ele () -> 
+      match ele with
+      | i when i - 1 > 0 && Char.equal (String.get sl (i - 1)) 'S' ->
+        set_bucket buckets (i - 1) (Array.get text (i - 1)) empty_slots 'S'
+      | _ -> ())
 
   let sais (text : 'a Array.t) : t =
     let buckets, starts, ends, empty_slots = make_buckets text in
