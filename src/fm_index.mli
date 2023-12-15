84eff52ef628849227b3c99abecbd06dbb416c07
drnatebrown <drnatebrown@gmail.com>
2023-12-14 22:17:51 -0500

    FM WIP

--- src/fm_index.mli
+++ src/fm_index.mli
@@ -1,25 +1,18 @@
 (** Implements an FM-index over a BWT, which supports exact pattern matching queries. Requires some module which can support
     rank, access, and select queries over characters and defines the text; such as a wavelet tree. **)
-module FM_index (_ : sig
-    val rank : char -> int -> int
-    val access : int -> char
-    val select : char -> int -> int
-  end) : 
-  sig
-    (** [count str] returns the number of occurrences of [str] within the text **)
-    val count : string -> int
-    (** [locate str] returns the locations (offsets) of occurrences of [str] within the text **)
-    val locate : string -> int list
-    (** [extract i m] returns the substring of length [m] starting from position [i] **)
-    val extract : int -> int -> string
-    (** [lf i] returns the position of the preceding character of BWT[i] wrt. the original text**)
-    val lf : int -> int
-    (** [lf i c] returns the position of the preceding character c of the first c before position i**)
-    val lf_c : int -> char -> int
-  end
-
+    module FM_index :
+    sig
+      type t
+      (** [construct str] returns the FM index object for [str]  **)
+      val construct : string -> t
+      (** [count str] returns the number of occurrences of [str] within the text **)
+      val exists : t -> string -> bool
+      (** [count index str] returns the number of occurrences of [str] within the [index] **)
+      val count : t -> string -> int
+    end
+  
 (* The R_index implements the same pattern matching as an FM-index, but does so in runs-compressed space *)
-module R_index (_ : sig
+(* module R_index (_ : sig
     val rank : char -> int -> int
     val access : int -> char
     val select : char -> int -> int
@@ -30,4 +23,4 @@ end) :
     val extract : int -> int -> string
     val lf : int -> int
     val lf_c : int -> char -> int
-end
+end *)
