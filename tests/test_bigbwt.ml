5ae3c205756a49b43d93e3d834a71db328563a18
drnatebrown <drnatebrown@gmail.com>
2023-12-15 02:33:48 -0500

    bigbwt tests

--- tests/test_bigbwt.ml
+++ tests/test_bigbwt.ml
@@ -2,13 +2,6 @@ open Core
 open OUnit2
 open BigBWT.Pfp
 
-module TestHash = struct
-  let is_trigger_string s =
-    List.mem [ "AC"; "AG"; "T+"; "$$" ] s ~equal:String.( = )
-end
-
-module Parser = PFP (TestHash)
-
 let paper_T = "GATTACAT+GATACAT+GATTAGATA"
 let paper_BWT = "ATTTTTTCCGGGGAAA+$+AAATATAA"
 
@@ -28,19 +21,6 @@ let paper_RLBWT =
     ('T', 1);
     ('A', 2);
   ]
-
-let paper_D = [ "$GATTAC"; "ACAT+"; "AGATA$$"; "T+GATAC"; "T+GATTAG" ]
-let paper_parse = [ 0; 1; 3; 1; 4; 2 ]
-let occ = [ 1; 2; 1; 1; 1 ]
-
-let test_window _ =
-  assert_equal (Parser.parse paper_T 2) (paper_D, occ, paper_parse)
-
-let test_paper_bwt _ = assert_equal (Parser.getBWT paper_T 2) paper_BWT
-
-let pfp_tests =
-  "pfp_tests"
-  >::: [ "paper test" >:: test_window; "paper bwt" >:: test_paper_bwt ]
 
 let seq =
   In_channel.with_file "./data/big.txt" ~f:(fun channel ->
@@ -133,5 +113,5 @@ let sa_int_tests =
   "sa_int_tests"
   >::: [ "sais sa" >:: test_sais_sa; "gsacak sa" >:: test_gsacak_sa ]
 
-let series = "Project Tests" >::: [ pfp_tests; bwt_tests; sa_int_tests ]
+let series = "Project Tests" >::: [ bwt_tests; sa_int_tests ]
 let () = run_test_tt_main series
